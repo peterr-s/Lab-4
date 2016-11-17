3abc61b68176fc00b56230f851c07f4fff527d5d
alon464 <alon464@192.168.29.134>
2016-11-17 16:37:44 +0100

    Hypothesis 1 should check if methods really needed or something is missing here

--- src/de/ws1617/pccl/search/Hypothesis.java
+++ src/de/ws1617/pccl/search/Hypothesis.java
@@ -1,8 +1,57 @@
 package de.ws1617.pccl.search;
 
-public class Hypothesis {
+import java.util.ArrayList;
 
-	// TODO implememnt me !
+import de.ws1617.pccl.fsa.Edge;
+
+public class Hypothesis
+{
+	private int state;
+	private int inputIndex;
+	private ArrayList<Edge>edges;//not sure about that but i think somehow it should
+	//show all edges processed till now , nop?!
+	private ArrayList<Integer> index;
 	
+	
+	/**
+	 * Constructor 
+	 * @param s the state 
+	 * @param i
+	 */
+	public Hypothesis(int s ,int i)
+	{
+		state=s;
+		inputIndex=i;
+		index=new ArrayList<>();
+		
+	}
 
+	public void setIndex(ArrayList<Integer>in)
+	{
+		index=in;
+	}
+	public int getState()
+	{
+		return state;
+	}
+	
+	public int getInputIndex()
+	{
+		return inputIndex;
+	}
+	/**
+	 * clones the hypothesis adding all index we had so far
+	 */
+	public Hypothesis clone()
+	{
+		Hypothesis h = new Hypothesis(state,inputIndex);
+		ArrayList<Integer>array=new ArrayList<>();
+		for(Integer i : index)
+		{
+			array.add(i);
+		}
+		h.setIndex(array);
+		return h;
+	}
+	
 }
