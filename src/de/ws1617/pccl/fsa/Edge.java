c8997ccd1f4281c10c6505412887d81adae1d437
alon464 <alon464@192.168.29.134>
2016-11-17 16:45:55 +0100

    edge commented

--- src/de/ws1617/pccl/fsa/Edge.java
+++ src/de/ws1617/pccl/fsa/Edge.java
@@ -11,6 +11,12 @@ public class Edge {
 	int goal;
 	Terminal toConsume;
 	
+	/**
+	 * Constructor creates the Edge giving it an index
+	 *  and the content of terminal 
+	 * @param g the goal index of Terminal
+	 * @param t the terminal stored in this edge 
+	 */
 	public Edge(int g , Terminal t)
 	{
 		goal=g;
